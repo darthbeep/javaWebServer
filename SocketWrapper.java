e841adcd815863c3b3d985b4f50c70cdab3a0632
darthbeep <darthbeep@gmail.com>
2019-01-18 02:03:59 -0500

    working echo server

--- SocketWrapper.java
+++ SocketWrapper.java
@@ -4,7 +4,6 @@ import java.io.*;
 public class SocketWrapper {
 
   int port;
-
   private ServerSocket serverSocket;
 
 
@@ -16,10 +15,28 @@ public class SocketWrapper {
     try {
       serverSocket = new ServerSocket(port);
       Socket clientSocket = serverSocket.accept();
-      System.out.println(clientSocket.getOutputStream());
+      //System.out.println(clientSocket.getOutputStream());
+      interperateSocket(clientSocket);
     }
     catch(IOException e) {
       System.out.println("yep");
     }
   }
+
+  private void interperateSocket(Socket clientSocket) {
+    try {
+      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+      String inputLine;
+      while ((inputLine = in.readLine()) != null) {
+        out.println(inputLine);
+      }
+    }
+    catch(IOException e) {
+      System.out.println("idk");
+    }
+  }
+
+
 }
